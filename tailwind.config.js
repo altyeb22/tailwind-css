e3fd07a69d8ac971d962e19ad25561da2eb3db80
alty7b22 <alty7b22@gmail.com>
2023-09-25 17:47:59 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // colors :{
+    //   transparent: 'transparent',
+    //   'yellowmy':{
+    //     100:'#f6d400',
+    //     200:'#f4cc07',
+    //     300:'#d97706',
+    //   },
+      
+    //   'bulemy':{
+    //    100: '#2f5cbb',
+    //    200:'#1e3a8a',
+    //   },
+    //   'black':'#000',
+    //   'white':'#fff',
+      
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
+
